29ad239ed381636ceed25775d0495c6b19fc6c44
sodayoung <729185278@qq.com>
2019-09-05 16:07:32 +0800

    查询数据库验证表单demo

---
+++ src/com/xut/javaweb/LoginServlet.java
@@ -0,0 +1,77 @@
+package com.xut.javaweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String sql = "SELECT count(id) FROM student WHERE name = ? " +
+				"AND password = ?";
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		PrintWriter out = response.getWriter();
+		
+		try {
+			String url = "jdbc:mysql://localhost:3306/user?serverTimeZone=Shanghai&useUnicode=true&characterEncoding=utf8&useSSL=false";
+			String user = "root";
+			String psw = "123456";
+			response.setContentType("text/html;charset=UTF-8");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(url,user,psw);
+			stmt = connection.prepareStatement(sql);
+			stmt.setString(1, username);
+			stmt.setString(2, password);
+			rs = stmt.executeQuery();
+			if (rs.next()) {
+				int count = rs.getInt(1);
+				if (count>0) {
+					out.print("hello"+username);
+				}else {
+					out.print("sorry"+username);
+				}
+			}
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}finally {
+			if (rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			if (stmt!=null) {
+				try {
+					stmt.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+			if (connection!=null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
